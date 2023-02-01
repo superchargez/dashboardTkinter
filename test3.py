41cf129ebfe0296e1651ac60669c000040780a31
superchargez <87804931+superchargez@users.noreply.github.com>
2023-02-01 16:31:03 +0500

    Add files via upload
    smarter code

---
+++ test3.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+
+from PIL import Image, ImageTk
+import pandas as pd
+import numpy as np
+
+tabel = pd.read_excel('testdata.xlsx', sheet_name='denied', index_col='Region')
+print(tabel)
+root = tk.Tk()
+root.option_add("*Label*sticky", 'ew')
+
+redDown = r"arrows/small-red-down.png"
+redUp = r"arrows/small-red-up.png"
+greenDown = r"arrows/small-green-down.png"
+greenUp = r"arrows/small-green-up.png"
+grayArrow = r"arrows/small-gray-right.png"
+
+def arrowDenied(value=-0):
+    if value < -0.5:
+        arrow = ImageTk.PhotoImage(Image.open(greenDown))
+    elif value > 0.5:
+        arrow = ImageTk.PhotoImage(Image.open(redUp))
+    else: arrow = ImageTk.PhotoImage(Image.open(grayArrow))
+    return arrow
+
+for i in range(3):
+    for j in range(4):
+        label = tk.Label(root, text=tabel.iloc[i][j]).grid(row=i,column=j)
+        print(type(tabel.iloc[i][j]))
+        # myimg1 = arrowDenied(tabel.loc[i][j])
+        # panel12 = tk.Label(root, image = myimg1).grid(row=i, column=j+1)
+
+root.mainloop()
