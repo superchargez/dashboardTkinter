775ebeb9697d2ceb6f2cd6e9f39d52cf04b2cd0b
superchargez <87804931+superchargez@users.noreply.github.com>
2023-02-05 22:24:09 +0500

    amended the code

--- addLoopedFrames.py
+++ addLoopedFrames.py
@@ -3,27 +3,41 @@ from tkinter import *
 import pandas as pd
 import openpyxl as xl
 
-path_to_excel = r"testdata.xlsx"
-wb = wb = xl.load_workbook(path_to_excel)
-sheets = wb.sheetnames
+# path_to_excel = r"testdata.xlsx"
+# wb = wb = xl.load_workbook(path_to_excel)
+# sheets = wb.sheetnames
 # for i in sheets:
 #     dfs = [pd.read_excel(path_to_excel, sheet_name=i, skiprows=0) for i in sheets]
 
 root = Tk()
-root.config(padx=10, pady=10)
+redDown = PhotoImage(file="arrows/small-red-down.png")
+redUp = PhotoImage(file="arrows/small-red-up.png")
+greenDown =PhotoImage(file="arrows/small-green-down.png")
+greenUp = PhotoImage(file="arrows/small-green-up.png")
+grayArrow = PhotoImage(file="arrows/small-gray-right.png")
+def redDown(value=0):
+    return redDown if value < -0.5 else greenUp if value > 0.5 else grayArrow
+def greenDown(value=0):
+    return greenDown if value < -0.5 else redUp if value > 0.5 else grayArrow
+
+# root.config(padx=10, pady=10)
 root.title("KPI Dashboard")
 products = ['xDSL', 'FF', 'Voice', 'IPTV']
-colours = ["powder blue", 'bisque', "RosyBrown1", "khaki2"]
+colours = ["powder blue", 'bisque', "RosyBrown1", "khaki2", "light sea green"]
+cursor = ['circle red', 'sizing red', 'target red', 'exchange red']
+rows, columns = ['North', 'South', 'Central', 'National'], ['Current', 'Last Month', 'Last Year', 'Target']
 counter = 0
 frames = []
 for i in range(4):
     for j in range(4):
-        frames.append({''.join(["frame",str(i),str(j)]):LabelFrame(root, width=270, height=130, text = products[j],
-            # bg="light sea green",
-            bg=colours[j],
-            relief=RAISED, borderwidth=5,
-            cursor="circle red").grid(row=i,column=j, sticky="EW")})
+        frame = LabelFrame(root, width=270, height=130, text = products[j],
+        bg=colours[j], relief=RAISED, borderwidth=5, cursor="circle red")
+        frames.append(frame)
+        frame.grid(row=i,column=j, sticky="EW")
+        Label(frames[counter], text=rows[j], borderwidth=0, cursor=cursor[i]).grid(row=0,column=1, sticky="EW")
+        Label(frames[counter], image=greenDown(), borderwidth=0, cursor=cursor[i]).grid(row=i,column=j, sticky="EW")
 
         counter+=1
-print(type(frames[0]))
+
+print(frames)
 root.mainloop()
