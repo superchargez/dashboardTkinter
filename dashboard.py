574d6058f564719fccaa25018b5d555dd01bd5c1
superchargez <87804931+superchargez@users.noreply.github.com>
2023-02-09 23:23:52 +0500

    fixed bug
    completed dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,11 @@
-import os, sys, tkcap
 from tkinter import *
 import pandas as pd
 import openpyxl as xl
-
 path_to_excel = r"testdata.xlsx"
 wb = wb = xl.load_workbook(path_to_excel)
 sheets = wb.sheetnames
 for i in sheets:
     dfs = [pd.read_excel(path_to_excel, sheet_name=i, skiprows=0) for i in sheets]
-
 root = Tk()
 redDown = PhotoImage(file="arrows/small-red-down.png")
 redUp = PhotoImage(file="arrows/small-red-up.png")
@@ -33,14 +30,20 @@ for i in range(4):
         bg=colours[j], relief=RAISED, borderwidth=5, cursor="circle red")
         frames.append(frame)
         frame.grid(row=i,column=j, sticky="EW")
-        Label(frames[counter], font=('Times New Roman', 8, 'bold'), text=calls[i], relief=RAISED, bg="sky blue", borderwidth=2, cursor=cursor[i]).grid(row=0,column=0, sticky="EW")
+        Label(frames[counter], font=('Times New Roman', 8, 'bold'), text=calls[i], relief=RAISED,
+        bg="sky blue",
+        borderwidth=2, cursor=cursor[i]).grid(row=0,column=0, sticky="EW")
         for k in range(4):
-            Label(frames[counter], text=counter, relief=RAISED, borderwidth=2, cursor=cursor[i], width=10, bg="light yellow").grid(row=0,column=k*2+1, sticky="EW", columnspan=2)
-            Label(frames[counter], text=rows[k], borderwidth=2, pady=1, relief=RAISED, cursor=cursor[i], width=8, bg="light pink").grid(row=k+1,column=0, sticky="EW", columnspan=1)
-            data = dfs[counter].set_index('Region')
-            value = data.iloc[j][k]
-            myimg1 = greenDownf(value)
+            Label(frames[counter], text=counter, relief=RAISED, borderwidth=2, cursor=cursor[i], width=10,
+            bg="light yellow",
+            ).grid(row=0,column=k*2+1, sticky="EW", columnspan=2)
+            Label(frames[counter], text=rows[k], borderwidth=2, pady=1, relief=RAISED, cursor=cursor[i], width=8,
+            bg="light pink",
+            ).grid(row=k+1,column=0, sticky="EW", columnspan=1)
             for m in range(4):
+                data = dfs[counter].set_index('Region')
+                value = data.iloc[k][m]
+                myimg1 = greenDownf(value)
                 Label(frames[counter], text=value,bg=colours[j]).grid(row=k+1,column=m*2+1, sticky="E", columnspan=1) # numbers in the table
                 if m == 0: continue
                 Label(frames[counter], fg = 'red', font="bold", bg=colours[j], image=myimg1).grid(row=k+1,column=m*2+2, sticky="W", columnspan=1) # numbers in the table
